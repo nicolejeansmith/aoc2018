24ae7d3c152c344ee12b9090d47ffc9d7a8f10f3
Nicole Jean Smith <nicole.j.smith@gmail.com>
2018-12-04 13:00:29 -0500

    day 1 part 2

--- day1/day1.py
+++ day1/day1.py
@@ -1,3 +1,4 @@
+# Part One
 def adjust_freq(sequence):
     freq = 0
     for change in sequence:
@@ -5,12 +6,38 @@ def adjust_freq(sequence):
     return (freq)
 
 
-def test():
+def test1():
     assert adjust_freq((1, -2, 3, 1)) == 3
-    assert adjust_freq((1,1,1)) == 3
+    assert adjust_freq((1, 1, 1)) == 3
     assert adjust_freq((1, 1, -2)) == 0
     assert adjust_freq((-1, -2, -3)) == -6
 
+test1()
+
 with open('./input.txt') as file:
     input = [int(line.strip()) for line in file.readlines()]
-    print(adjust_freq(input))
+
+print(adjust_freq(input))
+
+# Part Two
+def find_first_dupe(sequence):
+    freq = 0
+    display = set([0])
+    index = 0
+    size = len(sequence)
+    while len(display) == index + 1:
+        freq += sequence[index % size]
+        display.add(freq)
+        index += 1
+    return(freq)
+
+def test2():
+    assert find_first_dupe((1, -2, 3, 1)) == 2
+    assert find_first_dupe((1, -1)) == 0
+    assert find_first_dupe((3, 3, 4, -2, -4)) == 10
+    assert find_first_dupe((-6, 3, 8, 5, -6)) == 5
+    assert find_first_dupe((7, 7, -2, -7, -4)) == 14
+
+test2()
+
+print(find_first_dupe(input))
